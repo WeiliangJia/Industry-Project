5789845dd50d945e6392e2a11b97bba358a64062
WeiliangJia <1144890814@qq.com>
2024-08-13 14:42:59 +0930

    create first rep

---
+++ model.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+# print(tf.__version__)
+from tensorflow import keras
+# print(keras.__version__)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+# Install opencv using "pip install opencv-python" in order to use cv2.
+import cv2
+
+
+
+
