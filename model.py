bc51f3e4e9a24d7ea3944977e6aa9d09a6d7ec8d
WeiliangJia <1144890814@qq.com>
2024-08-18 11:47:42 +0930

    hdr img read complete

--- model.py
+++ model.py
@@ -6,10 +6,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-
-# Install opencv using "pip install opencv-python" in order to use cv2.
+import nibabel as nib
 import cv2
 
+img1 = nib.load(r'C:\Users\a\Desktop\3021 Industry Project\github folder\Industry-Project\image file\cbv.img')
+hdr1 = nib.load(r'C:\Users\a\Desktop\3021 Industry Project\github folder\Industry-Project\image file\cbv.hdr')
+
+print(img1)
+print(hdr1)
 
 
 
