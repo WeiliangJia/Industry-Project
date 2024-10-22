c1c3f1d6f7dab852dd67cbff8fb5508bfd5fd464
PIPIPIPIpppp <100545742+PIPIPIPIpppp@users.noreply.github.com>
2024-10-22 15:25:05 +1030

    Update Observation.py

--- Observation.py
+++ Observation.py
@@ -1,3 +1,16 @@
+import tensorflow as tf
+# print(tf.__version__)
+from tensorflow import keras
+# print(keras.__version__)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import nibabel as nib
+import cv2
+import mimetypes
+
+
 #observation
 def observation_tumour(masked_area):
     #先看轮廓
